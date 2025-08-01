fc66ad46eafd902cd288373ab02d673ecae555e2
hrou0003 <harrisonrouillard@gmail.com>
2025-08-01 09:22:06 -0400

    .

--- test/TestApp/obj/Debug/net9.0/TestApp.AssemblyInfo.cs
+++ test/TestApp/obj/Debug/net9.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f194bd46eea3a81a7e992457b2d07628fa272873")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
